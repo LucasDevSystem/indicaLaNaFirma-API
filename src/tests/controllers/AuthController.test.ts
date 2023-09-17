f53a66d2f8a0d2dda0dbf6de2c149c46db41ac62
LucasDevSystem <highspeedsetup@gmail.com>
2023-09-16 21:31:41 -0300

    Feat (Authcontroller.test) test script

---
+++ src/tests/controllers/AuthController.test.ts
@@ -0,0 +1,12 @@
+const { describe, it } = require("@jest/globals");
+const request = require("supertest");
+ 
+// Auth controller test script 
+// full branches coverage 
+// incomplete fields
+// existing user
+// inexisting user
+describe("Test authController", () => {
+  it("Create account with incomplete fields", async () => {
+  });
+});
